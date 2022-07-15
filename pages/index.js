a809cd02182251b796902335b64a7dac36813c67
RyanSyme <ryu321@hotmail.com>
2022-07-15 15:51:52 +0200

    create jsx pages, import babel

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
+import { Cart, Footer, FooterBanner, HeroBanner, Layout, Navbar, Product } from '../components'
+
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
+      <div className='products-container'>
         {['product 1','product 2'].map((product) => (
           <div>{product}</div>
         ))}
       </div>
-      
-      Footer
+
+      <FooterBanner/>
     </>
   )
 }

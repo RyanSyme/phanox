d15a456a0176cec3ad9903be6aba41c2a96b9623
RyanSyme <ryu321@hotmail.com>
2022-07-29 16:59:51 +0200

    add showCart functionality to Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
 
+import { Cart } from './'
+import { useStateContext } from '../context/StateContext'
+
 const Navbar = () => {
+
+  const { showCart, setShowCart, totalQuantities } = useStateContext()
+
   return (
     <div className='navbar-container'>
       <p className='logo'>
@@ -11,11 +17,13 @@ const Navbar = () => {
       <button 
         type='button' 
         className='cart-icon' 
-        onClick=''
+        onClick={() => setShowCart((prevCart)=> !prevCart)}
       >
         <AiOutlineShopping/>
-        <span className='cart-item-qty'>10</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart/>}
     </div>
   )
 }

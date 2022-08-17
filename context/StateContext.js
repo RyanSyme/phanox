e937eba74bae054453c1b8ed2dc0264b2a5a051d
RyanSyme <ryu321@hotmail.com>
2022-08-17 15:20:18 +0200

    set up conection to stripe via strip.js

--- context/StateContext.js
+++ context/StateContext.js
@@ -3,7 +3,7 @@ import { toast } from 'react-hot-toast'
 
 const Context = createContext()
 
-// passes cart info through site through SateContext using context.Provider
+// passes cart info through site via StateContext using context.Provider
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false)
     const [cartItems, setCartItems] = useState([])
---
+++ pages/api/stripe.js
@@ -0,0 +1,35 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const params = {
+                submit_type: 'pay',
+                payment_method_types: ['card'],
+                billing_address_collection: 'auto',
+                shipping_options: [
+                    { shipping_rate: 'shr_1LXm71IxSQF4HiQYNRLCy63a' },
+                    { shipping_rate: 'shr_1LXm5IIxSQF4HiQYONAeRgzj' }
+                ],
+                line_items: [
+                    {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    price: '{{PRICE_ID}}',
+                    quantity: 1,
+                    },
+                ],
+                mode: 'payment',
+                success_url: `${req.headers.origin}/?success=true`,
+                cancel_url: `${req.headers.origin}/?canceled=true`,
+                }
+            // Create Checkout Sessions from body params.
+            const session = await stripe.checkout.sessions.create(params);
+            res.redirect(303, session.url);
+        } catch (err) {
+            res.status(err.statusCode || 500).json(err.message);
+            }
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+}
